a1a274fb2e83735abba598ff209d4fa3558f1f65
aydencodes <84056225+aydencodes@users.noreply.github.com>
2021-06-24 19:29:18 -0700

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,65 @@
+
+class paper
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+		
+		
+        this.image=LoadImage("paper.png")
+		
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(this.angle)
+			//rect(0,0,this.wallThickness, this.paperHeight);
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			//rect(0,0,this.wallThickness, this.paperHeight);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			//rect(0,0,this.paperWidth, this.wallThickness);
+			pop()
+			
+	}
+
+}
